f79e301ceb6afcfb1f99486669f5e46fafc74d08
Bikash <dasbikash2258@gmail.com>
2021-09-18 11:54:16 +0530

    added sum of 3 num method

--- calc.py
+++ calc.py
@@ -1,6 +1,13 @@
 def sum(a: int, b: int) -> int:
- return a + b;
+ return a + b
  
+
+def sumThreeNum(a: int, b: int, c: int) -> int:
+ return a+b+c
 
 s = sum(10,20)
 print("Sum: ", s)
+
+
+s3 = sumThreeNum(10,20,30)
+print("Sum of three num: ", s3)
